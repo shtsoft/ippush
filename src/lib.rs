d9eae05bd0f359d86ee981eeb84752b1337756f2
shtsoft <git@shtsoft.eu>
2024-07-05 22:43:44 +0200

    Add documentation according to CONTRIBUTING.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! ippush is a remote procedure returning the callers IP address and a timestamp.
+
 use std::env::Args;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
 
@@ -5,11 +7,19 @@ use chrono::prelude::Utc;
 
 use serde::Serialize;
 
+/// Abstracts the configuration of the application.
 pub struct Config {
+    /// socket address to which the application binds
     addr: SocketAddr,
 }
 
 impl Config {
+    /// Creates a new configuration of the application.
+    /// - `args` are the commandline parameters made up by the IP and port to which the application should bind.
+    ///
+    ///  # Errors
+    ///
+    ///  An error is returned if the number of arguments is wrong or if parsing the arguments fails.
     pub fn new(mut args: Args) -> Result<Self, Box<dyn std::error::Error>> {
         args.next();
 
@@ -33,12 +43,19 @@ impl Config {
     }
 }
 
+/// Abstracts the return value of the remote procedure.
 #[derive(Serialize)]
 struct ReturnValue {
     ip: IpAddr,
     time: String,
 }
 
+/// Run the application.
+/// - `config` is the configuration the application is run in.
+///
+///  # Errors
+///
+///  An error is returned if writing or reading the socket fails.
 #[allow(clippy::needless_pass_by_value)]
 pub fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     let socket = UdpSocket::bind(config.addr)?;
